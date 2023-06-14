386e1c0cd01e7b0313cb14a29bf1e195ce3c59ef
Zoya Afreen <zoyafreen04@gmail.com>
2023-06-15 01:29:15 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Zoya Afreen on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
